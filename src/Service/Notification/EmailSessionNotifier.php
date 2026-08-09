825751617ebe3279f43763cec6fa2f382d94ee5b
mickaelboonen <mickaelboonen@gmail.com>
2026-08-09 13:50:01 +0200

    feature email : update sender

--- src/Service/Notification/EmailSessionNotifier.php
+++ src/Service/Notification/EmailSessionNotifier.php
@@ -2,12 +2,16 @@
 
 namespace App\Service\Notification;
 
+use Symfony\Component\Mime\Address;
 use App\Entity\GameSession;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
 
 final readonly class EmailSessionNotifier implements SessionNotifierInterface
 {
+    private const SENDER_EMAIL = 'campaign-planner@alwaysdata.net';
+    private const SENDER_NAME = 'CampaignPlanner';
+
     public function __construct(
         private MailerInterface $mailer,
     ) {
@@ -21,7 +25,7 @@ final readonly class EmailSessionNotifier implements SessionNotifierInterface
             }
 
             $email = (new TemplatedEmail())
-                ->from('campaign-planner@alwaysdata.net')
+                ->from($this->getSender())
                 ->to($participant->getEmail())
                 ->subject($this->setMailSubject('Nouvelle session planifiée', $session))
                 ->htmlTemplate('emails/session_scheduled.html.twig')
@@ -43,7 +47,7 @@ final readonly class EmailSessionNotifier implements SessionNotifierInterface
             }
 
             $email = (new TemplatedEmail())
-                ->from('campaign-planner@alwaysdata.net')
+                ->from($this->getSender())
                 ->to($participant->getEmail())
                 ->subject($this->setMailSubject('Session annulée', $session))
                 ->htmlTemplate('emails/session_cancelled.html.twig')
@@ -65,7 +69,7 @@ final readonly class EmailSessionNotifier implements SessionNotifierInterface
             }
 
             $email = (new TemplatedEmail())
-                ->from('campaign-planner@alwaysdata.net')
+                ->from($this->getSender())
                 ->to($participant->getEmail())
                 ->subject($this->setMailSubject('Rappel — Session à venir', $session))
                 ->htmlTemplate('emails/session_reminder.html.twig')
@@ -79,8 +83,13 @@ final readonly class EmailSessionNotifier implements SessionNotifierInterface
         }
     }
 
-     private function setMailSubject(string $type, GameSession $session): string
-     {
-        return sprintf('%s — %s', $type, $session->getCampaign()->getName());
-     }
+    private function setMailSubject(string $type, GameSession $session): string
+    {
+    return sprintf('%s — %s', $type, $session->getCampaign()->getName());
+    }
+
+    private function getSender(): Address
+    {
+        return new Address( self::SENDER_EMAIL, self::SENDER_NAME);
+    }
 }
