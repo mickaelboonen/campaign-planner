d515db276d93d3d5b13ae4062a0da47c6f29ebdf
mickaelboonen <mickaelboonen@gmail.com>
2026-08-11 23:48:25 +0200

    correction : deleted dump

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,7 +12,6 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        dump($this->getUser());
         if ($this->getUser() !== null) {
             return $this->redirectToRoute('dashboard');
         }
